b50823f0b405a4de9a6e4f791a6ba0b8d947a1f1
nico <79845116+niconova123@users.noreply.github.com>
2021-03-14 19:40:29 +0700

    menambahkan while loop

--- minggu ke-2/latihan/loop.php
+++ minggu ke-2/latihan/loop.php
@@ -1,8 +1,22 @@
 <?php
+// pake for //
 for ($i = 1; $i <=5; $i++){
     for($j=1; $j<=$i; $j++){
         echo $i;
     }
     echo "<br>";
 }
+// pake while //
+$i= 1;
+while ($i<=5){
+    echo "<br>";
+    $j=1;
+    while ($j<=$i){
+        echo $i;
+        $j+=1;
+
+    }
+    $i+=1;
+}
+    
 ?>
