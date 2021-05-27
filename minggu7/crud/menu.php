5738bdf761b022d2dd5234e10e269defbafe9da6
nico <79845116+niconova123@users.noreply.github.com>
2021-05-27 19:25:43 +0700

    Delete minggu7 directory

--- minggu7/crud/menu.php
+++
@@ -1,6 +0,0 @@
-<div class="menu">
-    <ul>
-        <li><a href="index.php?p=home">Home</a></li>
-        <li><a href="index.php?p=sekolah">Sekolah</a></li>
-    </ul>
-</div>
--- minggu7/latihan/functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-$sambung_ke_db = mysqli_connect("localhost", "root", "", "minggu7");
-
-if (!$simpanData) {
-    echo mysqli_error($sambung_ke_db);
-}
-?>
